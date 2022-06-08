d1a0afae7c6e08fe54c177612feb568c01adc6c7
Yifan <datasciencewyfan@gmail.com>
2022-06-08 15:31:25 -0700

    commit tree 7f52b80b9e24e742f12f5768cc6d658ed4355007

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React , {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [isClicked , setIsClicked] = useState(false)
+
+  function onSetIsClicked(){
+    setIsClicked(isClicked => !isClicked)
+  }
+
+  const itemClass = isClicked ? 'in-cart' : ''
+  const buttonText = isClicked ? 'Remove From Cart' : 'Add to Cart'
+  const buttonClass = isClicked ? 'remove' : 'add'
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={onSetIsClicked}>{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React , {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  
+  const [category , setCategory] = useState ('All')
+
+  function onSetCategory(e){
+    setCategory(e.target.value)
+  }
+  
   return (
     <div className="ShoppingList">
+      
       <div className="Filter">
-        <select name="filter">
+        
+        <select name="filter" onChange = {(e)=> onSetCategory(e)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
+      
       </div>
+      
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.filter((item)=>{
+          if (category === 'All'){
+            return true
+          } else{
+            return item.category === category
+          } 
+        }).map((item) => {
+          return <Item key={item.id} name={item.name} category={item.category} />
+        })}
       </ul>
+    
     </div>
   );
 }
